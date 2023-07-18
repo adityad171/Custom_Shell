7bf13f6c13a4f750a8f9b2e3576b688b0d22182c
adityad171 <97349366+adityad171@users.noreply.github.com>
2023-07-18 19:52:09 +0530

    Delete pwd.c

--- Aditya_2021228_Shell/pwd.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-#include <stdio.h>
-int main() {
-    char cwd[1024];
-    chdir("change dir to");
-    getcwd(cwd, sizeof(cwd));
-    printf("Current working dir: %s\n", cwd);
-}
