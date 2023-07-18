5075faeaaaa3608db926188d4ad41f33574e9879
adityad171 <97349366+adityad171@users.noreply.github.com>
2023-07-18 19:51:59 +0530

    Update rm.c

--- Aditya_2021228_Shell/rm.c
+++ Aditya_2021228_Shell/rm.c
@@ -1,12 +1,39 @@
 #include <stdio.h>
-int main() {
-     char str[20];
-    printf("Enter the name of the File: ");
-    scanf("%s",str);
-    if (remove(str) != 0) {
-        printf("file not deleted");
-    } else {
-        printf("file deleted.");
-    }
-    return 0;
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv){
+	if(argc==1){
+		printf("No file mentioned\n");
+	}
+	else{
+		if(strcmp(argv[1],"-i")==0){
+			for(int i=2;i<argc;i++){
+				printf("Remove file '%s'?",argv[i]);
+				char in[10]={'\0'};
+				scanf("%s",in);
+				if(strcmp(in,"y")==0){
+					int status = remove(argv[i]);
+					if(status!=0){
+						perror("rm failed");
+						exit(1);
+					}
+				}
+			}
+		}
+		else if(strcmp(argv[1],"-f")==0){
+			for(int i=1;i<argc;i++){
+                                int status = remove(argv[i]);
+                        }
+		}
+		else{
+			for(int i=1;i<argc;i++){
+				int status = remove(argv[i]);
+				if(status!=0){
+					perror("rm failed");
+					exit(1);
+				}
+			}
+		}
+	}
 }
